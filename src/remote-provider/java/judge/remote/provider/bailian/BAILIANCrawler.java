bb920dd0db38d2578a06e6e568ac4f86cccb2e02
sdibt <sdibtacm@126.com>
2017-05-04 21:56:24 +0800

    bug fix

--- src/remote-provider/java/judge/remote/provider/bailian/BAILIANCrawler.java
+++ src/remote-provider/java/judge/remote/provider/bailian/BAILIANCrawler.java
@@ -36,17 +36,11 @@ public class BAILIANCrawler extends SimpleCrawler {
         info.timeLimit = (Integer.parseInt(Tools.regFind(html, "<dd>\\s*(\\d+)ms\\s*</dd>")));
         info.memoryLimit = (Integer.parseInt(Tools.regFind(html, "<dd>\\s*(\\d*)kB\\s*</dd>")));
         info.description = (Tools.regFind(html, "<dt>\\s*描述\\s*</dt><dd>([\\s\\S]+?)</dd><dt>\\s*输入"));
-        
         info.input = (Tools.regFind(html, "<dt>\\s*输入\\s*</dt><dd>([\\s\\S]+?)</dd><dt>\\s*输出"));
-        
         info.output = (Tools.regFind(html, "<dt>\\s*输出\\s*</dt><dd>([\\s\\S]+?)</dd><dt>\\s*样例输入"));
-        
         info.sampleInput = (Tools.regFind(html, "<dt>\\s*样例输入\\s*</dt><dd>(<pre>[\\s\\S]+?</pre>)</dd><dt>\\s*样例输出"));
-        
         info.sampleOutput = (Tools.regFind(html, "<dt>\\s*样例输出\\s*</dt><dd>(<pre>[\\s\\S]+?</pre>)</dd>"));
-        
-        info.hint = (Tools.regFind(html, "<dt>\\s*提示\\s*</dt><dd>([\\s\\S]+?)</dd><dt>"));
-        
+        info.hint = (Tools.regFind(html, "<dt>\\s*提示\\s*</dt><dd>([\\s\\S]+?)</dd>"));
         info.source = (Tools.regFind(html, "<dt>\\s*来源\\s*</dt><dd>([\\s\\S]+?)</dd>").trim());
     }
 
