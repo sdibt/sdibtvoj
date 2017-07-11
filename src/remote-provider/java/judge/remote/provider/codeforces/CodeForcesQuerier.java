fe2b35aef38092fef15f97c292895141cec7db98
sdibt <sdibtacm@126.com>
2017-07-11 21:50:30 +0800

    Update CodeForcesQuerier.java

--- src/remote-provider/java/judge/remote/provider/codeforces/CodeForcesQuerier.java
+++ src/remote-provider/java/judge/remote/provider/codeforces/CodeForcesQuerier.java
@@ -1,26 +1,26 @@
 package judge.remote.provider.codeforces;
 
+import java.io.IOException;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import judge.httpclient.DedicatedHttpClient;
-import judge.httpclient.HttpStatusValidator;
+import judge.httpclient.SimpleHttpResponse;
 import judge.httpclient.SimpleNameValueEntityFactory;
 import judge.remote.RemoteOjInfo;
 import judge.remote.account.RemoteAccount;
 import judge.remote.provider.codeforces.CodeForcesTokenUtil.CodeForcesToken;
-import judge.remote.querier.AuthenticatedQuerier;
-import judge.remote.status.RemoteStatusType;
-import judge.remote.status.SubmissionRemoteStatus;
-import judge.remote.status.SubstringNormalizer;
+import judge.remote.submitter.CanonicalSubmitter;
 import judge.remote.submitter.SubmissionInfo;
 
-import org.apache.commons.lang3.Validate;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+
 @Component
-public class CodeForcesQuerier extends AuthenticatedQuerier {
+public class CodeForcesSubmitter extends CanonicalSubmitter {
 
     @Override
     public RemoteOjInfo getOjInfo() {
@@ -28,43 +28,67 @@ public class CodeForcesQuerier extends AuthenticatedQuerier {
     }
 
     @Override
-    protected SubmissionRemoteStatus query(SubmissionInfo info, RemoteAccount remoteAccount, DedicatedHttpClient client) {
-        String html = client.get("/contest/" + info.remoteProblemId.replaceAll("\\D.*", "") + "/submission/" + info.remoteRunId, HttpStatusValidator.SC_OK).getBody();
+    protected boolean needLogin() {
+        return true;
+    }
+    @Override 
+   protected Integer getMaxRunId(SubmissionInfo info, DedicatedHttpClient client, boolean submitted) throws IOException {
+       // String html = client.get("/submissions/" + info.remoteAccountId).getBody();
+        //Matcher matcher = Pattern.compile("submissionId=\"(\\d+)\"(?:[\\s\\S](?!tr))*" + info.remoteProblemId).matcher(html);
+        //return matcher.find() ? Integer.parseInt(matcher.group(1)) : -1; 
+        String html = client.get("/problemset/status").getBody();
+        // System.out.print(html);
+        String num=info.remoteProblemId.replaceAll("\\D.*", "");
+        Matcher matcher = Pattern.compile("href=\"/contest/" + num + "/submission/\\d+\"\\ssubmissionId=\"(\\d+)\">[\\s\\S]*>" +  info.remoteAccountId + "</a>").matcher(html);
+        return matcher.find() ? Integer.parseInt(matcher.group(1)) : -1;
+    }
 
-        String regex = 
-                        "width:4em[\\s\\S]*?" +
-                        "<td>" + info.remoteRunId + "</td>\\s*" + // run id
-                        "<td>[\\s\\S]*?</td>\\s*" + // author
-                        "<td>[\\s\\S]*?</td>\\s*" + // problem id
-                        "<td>[\\s\\S]*?</td>\\s*" + // language
-                        "<td>([\\s\\S]*?)</td>\\s*" + // status
-                        "<td>([\\s\\S]*?)</td>\\s*" + // time
-                        "<td>([\\s\\S]*?)</td>"; // memory
-                        
-        Matcher matcher = Pattern.compile(regex).matcher(html);
-        Validate.isTrue(matcher.find());
+    @Override
+    protected String submitCode(SubmissionInfo info, RemoteAccount remoteAccount, DedicatedHttpClient client) throws IOException, InterruptedException {
+        CodeForcesToken token = CodeForcesTokenUtil.getTokens(client);
         
-        SubmissionRemoteStatus status = new SubmissionRemoteStatus();
-        status.rawStatus = matcher.group(1).replaceAll("<.*?>", "").trim();
-        status.executionTime = Integer.parseInt(matcher.group(2).replaceAll("\\D+", ""));
-        status.executionMemory = Integer.parseInt(matcher.group(3).replaceAll("\\D+", ""));
-        status.statusType = SubstringNormalizer.DEFAULT.getStatusType(status.rawStatus);
-        if (status.statusType == RemoteStatusType.CE) {
-            CodeForcesToken token = CodeForcesTokenUtil.getTokens(client);
-            HttpEntity entity = SimpleNameValueEntityFactory.create( //
-                    "submissionId", info.remoteRunId, //
-                    "csrf_token", token.csrf_token //
-            );
-            html = client.post("/data/judgeProtocol", entity).getBody();
-            status.compilationErrorInfo = "<pre>" + html.replaceAll("(\\\\r)?\\\\n", "\n").replaceAll("\\\\\\\\", "\\\\") + "</pre>";
-        }
+        HttpEntity entity = SimpleNameValueEntityFactory.create( //
+            "csrf_token", token.csrf_token, //
+            "_tta", token._tta, //
+            "action", "submitSolutionFormSubmitted", //
+            "submittedProblemCode", info.remoteProblemId, //
+            "programTypeId", info.remotelanguage, //
+            "source", info.sourceCode + getRandomBlankString(), //
+            "sourceFile", "", //
+            "sourceCodeConfirmed", "true", //
+            "doNotShowWarningAgain", "on" //
+        );
         
-        matcher = Pattern.compile("on test\\s*(\\d+)").matcher(status.rawStatus);
-        if (matcher.find()) {
-            status.failCase  = Integer.parseInt(matcher.group(1));
+        SimpleHttpResponse response = client.post(
+            "/problemset/submit?csrf_token=" + token.csrf_token,
+            entity
+        );
+        
+        if (response.getStatusCode() != HttpStatus.SC_MOVED_TEMPORARILY) {
+            if (response.getBody().contains("error for__programTypeId")) {
+                return "Language Rejected";
+            }
+            if (response.getBody().contains("error for__source")) {
+                return "Source Code Error";
+            }
+            throw new RuntimeException();
         }
-
-        return status;
+        return null;
     }
     
+    private String getRandomBlankString() {
+        String string = "\n";
+        int random = new Random().nextInt(Integer.MAX_VALUE);
+        while (random > 0) {
+            string += random % 2 == 0 ? ' ' : '\t';
+            random /= 2;
+        }
+        return string;
+    }
+    
+    @Override
+    protected long getSubmitReceiptDelay() {
+        return 30000;
+    }
+
 }
