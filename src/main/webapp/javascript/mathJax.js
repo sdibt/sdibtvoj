904a49968202aaf72c3425496f38e380577c31e6
Boxjan <Boxjan@users.noreply.github.com>
2018-08-03 21:33:48 +0800

    fix cf mathjax use

--- src/main/webapp/javascript/mathJax.js
+++ src/main/webapp/javascript/mathJax.js
@@ -1,6 +1,7 @@
 function mathJaxFunc(){
     if($('#useMathJax').prop('checked')){
         $('head').append('<script async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML"> </script>');
+        $('head').append('<script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [[\'$\',\'$\'],[\'$$$\',\'$$$\'],["\\\\(","\\\\)"]],displayMath:[[\'\\[\',\'\\]\'], [\'$$\',\'$$\'],[\'$$$$$$\',\'$$$$$$\']]}});</script>')
     } else {
         location.reload();
     }
